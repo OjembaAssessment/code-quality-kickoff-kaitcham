cb2f382d064d412fa395f00ef2ff82f7b275ed5a
kaitcham <chams4f@gmail.com>
2023-10-09 21:41:47 +0200

    Validate password based on required conditions

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -1,4 +1,4 @@
-export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
+export const forbiddenPasswords = ['amG84h6yeQ', 'mc9Q20pdjH', 'jnT6Q2f8U5'];
 
 /**
  * Checks if a given password is valid or invalid.
@@ -6,20 +6,22 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @param {string} password
  * @returns {boolean}
  */
-export default function isValidPassword(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
-  if (typeof password !== "string") password = String(password);
 
-  // * * * YOUR CODE GOES IN HERE ... * * *
-  /*
-   * if (password is not exactly 10 digits or characters...) {
-   *   return ...;
-   * }
-   *
-   * if (is not composed by digits and numbers) {
-   *   return ...;
-   * }
-   */
+function increasingOrDecreasingSequence(password) {
+  const sequence = '0123456789876543210';
+  const matches = password.match(/(\d){4}/g);
+  return matches?.some((match) => sequence.includes(match));
+}
+
+export default function isValidPassword(password = '') {
+  if (typeof password !== 'string') password = String(password);
+  if (!/^.{10}$/.test(password)) return false;
+  if (/\W/.test(password)) return false;
+  if (!/[A-Z]/.test(password)) return false;
+  if (!/[a-z]/.test(password)) return false;
+  if (!/[0-9]/.test(password)) return false;
+  if (forbiddenPasswords.includes(password)) return false;
+  if (increasingOrDecreasingSequence(password)) return false;
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
   return true;
