99593fb904940a2ac1b4ab314ed1e6e3f582a3f0
kaitcham <chamsf5@aol.com>
2023-10-25 09:13:20 +0200

    Recursively sum all elements in an array

--- src/sumWithRecursion.js
+++ src/sumWithRecursion.js
@@ -1,7 +1,6 @@
 // Implement a JavaScript function to find the sum of all elements in an array recursively.
 
 function sumAllElements(arr) {
-  result = 0;
-  if (!arr.length) return result;
-  return (result += arr.shift() + sumAllElements(arr));
+  if (!arr.length) return 0;
+  return arr.shift() + sumAllElements(arr);
 }
